d3d21b42aeb7daa832dc2f82536183f2218c5c46
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-07-01 17:59:38 +0100

    Tidied up code

--- EviesProject/Program.cs
+++ EviesProject/Program.cs
@@ -45,25 +45,7 @@ namespace EviesProject
                     Console.WriteLine("Time for a motivational message. ");
                 }
 
-                //else {
-                    //Console.WriteLine("You have exercised for " + userInput + " minutes today.");
-                    //int minutesExercised = int.Parse(userInput);
-                    //runningTotal = minutesExercised + runningTotal; 
-                    //Console.WriteLine("This brings your running total to... " + runningTotal + ". ");
-
-                    //if (minutesExercised < 5) {
-                        //Console.WriteLine("Believe in yourself gurrl. You can do better than that! Now sweat baby sweat!");
-                        //}
-                        //else if (minutesExercised > 30){
-                        //    Console.WriteLine("You've exercised for more than half an hour today... Keep it up and you'll be the next Chuck Norris in no time!");
-                        //}
-
-  
-
-
-
-               // } //end of else
-
+             
             } // end of while loop
 
 
