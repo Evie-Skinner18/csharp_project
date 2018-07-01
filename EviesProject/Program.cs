59c2d3237f2beed1b5bd5c0d2ed5f87c57fae0c5
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-07-01 15:21:49 +0100

    Basic exercise calculator built

--- EviesProject/Program.cs
+++ EviesProject/Program.cs
@@ -6,7 +6,14 @@ namespace EviesProject
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int runningTotal = 0;
+            Console.WriteLine("How many minutes have you exercised today? ");
+
+            string userInput = Console.ReadLine();
+            int minutesExercised = int.Parse(userInput);
+            Console.WriteLine("You have exercised for " + minutesExercised + " minutes today.");
+            runningTotal = minutesExercised + runningTotal;
+            Console.WriteLine("This brings your running total to... " + runningTotal + ". Keep it up!");
         }
     }
 }
