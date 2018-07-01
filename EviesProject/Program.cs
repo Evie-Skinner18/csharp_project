f464d3af66943d98ced73f771bced6b7d61516f8
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-07-01 17:50:15 +0100

    Trying to fix runningTotal

--- EviesProject/Program.cs
+++ EviesProject/Program.cs
@@ -20,26 +20,43 @@ namespace EviesProject
                     keepGoing = false;
                     Console.WriteLine("Seeya!");
                 }
-                else {
-                    int minutesExercised = int.Parse(userInput);
-                    Console.WriteLine("You have exercised for " + minutesExercised + " minutes today.");
-                    runningTotal = minutesExercised + runningTotal; 
-                    Console.WriteLine("This brings your running total to... " + runningTotal + ". ");
 
-                        if (minutesExercised < 5)
-                        {
+                else
+                {
+                    Console.WriteLine("You've exercised " + userInput + " minutes today. ");
+
+                    int minutes = int.Parse(userInput);
+                    runningTotal = minutes + runningTotal;
+                    Console.WriteLine("Your running total of minutes exercised is now " + runningTotal + ". ");
+                    if (minutes < 5)
+                    {
                         Console.WriteLine("Believe in yourself gurrl. You can do better than that! Now sweat baby sweat!");
-                        }
-                        else if (minutesExercised > 30)
-                        {
-                            Console.WriteLine("You've exercised for more than half an hour today... Keep it up and you'll be the next Chuck Norris in no time!");
-                        }
+                    }
+                    else if (minutes > 30)
+                    {
+                        Console.WriteLine("You've exercised for more than half an hour today... Keep it up and you'll be the next Chuck Norris in no time!");
+                    }
+
+                } //end of else
+
+                //else {
+                    //Console.WriteLine("You have exercised for " + userInput + " minutes today.");
+                    //int minutesExercised = int.Parse(userInput);
+                    //runningTotal = minutesExercised + runningTotal; 
+                    //Console.WriteLine("This brings your running total to... " + runningTotal + ". ");
+
+                    //if (minutesExercised < 5) {
+                        //Console.WriteLine("Believe in yourself gurrl. You can do better than that! Now sweat baby sweat!");
+                        //}
+                        //else if (minutesExercised > 30){
+                        //    Console.WriteLine("You've exercised for more than half an hour today... Keep it up and you'll be the next Chuck Norris in no time!");
+                        //}
 
   
 
 
 
-                } //end of else
+               // } //end of else
 
             } // end of while loop
 
@@ -53,3 +70,5 @@ namespace EviesProject
     } // end of class
 
 } //end of namespace
+
+
